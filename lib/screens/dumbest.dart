76557b4caaedbfc8ec82ea170c485fd89f33ec96
Patrick Kelly <patkellydesigns@gmail.com>
2019-10-27 20:08:40 -0600

    ok

--- lib/screens/dumbest.dart
+++ lib/screens/dumbest.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:provider/provider.dart';
-import 'package:trump_machine/screens/search_results.dart';
 
 import 'package:trump_machine/services/services.dart';
 import 'package:trump_machine/shared/bot_nav.dart';
